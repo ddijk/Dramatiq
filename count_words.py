9736ab31c2d8456695b2472fa5d2f2ea9e4f272c
Dick Dijk <dick@dijkrosoft.nl>
2022-03-17 15:35:02 +0100

    Switch from default broker (RabbitMQ) to Redis

--- count_words.py
+++ count_words.py
@@ -1,6 +1,10 @@
 import dramatiq
 import requests
 
+from dramatiq.brokers.redis import RedisBroker
+
+redis_broker = RedisBroker(host="localhost")
+dramatiq.set_broker(redis_broker)
 
 @dramatiq.actor
 def count_words(url):
