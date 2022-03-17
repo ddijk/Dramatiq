cfaa446da3628f08d13204732359b1cb0c283765
Dick Dijk <dick@dijkrosoft.nl>
2022-03-17 17:14:38 +0100

    use different queue

--- count_words.py
+++ count_words.py
@@ -6,7 +6,8 @@ from dramatiq.brokers.redis import RedisBroker
 redis_broker = RedisBroker(host="localhost")
 dramatiq.set_broker(redis_broker)
 
-@dramatiq.actor
+@dramatiq.actor(queue_name="abc")
+#@dramatiq.actor
 def count_words(url):
     response = requests.get(url)
     count = len(response.text.split(" "))
