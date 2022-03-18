59526ec9499bda4ab661718a3e6e41216cb041a4
Dick Dijk <dick@dijkrosoft.nl>
2022-03-18 15:10:25 +0100

    set namespace to '' to avoid 'dramatic' prefix in Redis keys

--- push_worker.py
+++ push_worker.py
@@ -4,17 +4,22 @@ import sys
 
 from dramatiq.brokers.redis import RedisBroker
 
-redis_broker = RedisBroker(host="localhost")
+redis_broker = RedisBroker(host="localhost", namespace="")
 dramatiq.set_broker(redis_broker)
 
 @dramatiq.actor(queue_name="pjq")
 #@dramatiq.actor
 def unified_push_notification_job_processor(post_msg):
-    print(f"This was posted {post_message}")
+    print(f"This was posted {post_msg}")
+
+@dramatiq.actor(queue_name="pjq")
+def handle(post_msg):
+    print(f"This was posted HANDLE {post_msg}")
 
 def main():
     naam = sys.argv[1] if len(sys.argv)>1 else 'japie'
-    handle.send({'naam': naam})
+    output = unified_push_notification_job_processor.send({'naam': naam})
+    print(f'zegt {output}')	
 
 if __name__ == '__main__':
     main()
